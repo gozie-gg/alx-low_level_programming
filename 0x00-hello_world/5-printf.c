e867f05d9adb464206f346eb10acd462b3f6e47e
“gozie-gg” <goziegoodnews@gmail.com>
2022-12-10 15:26:33 -0800

    description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints the phrase with proper grammar,
- *but the outcome is art,
- *Return : 0 if successful
+ * but the outcome is art,
+ * Return : 0 if successful
  */
-int main(void)
+int main()
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
