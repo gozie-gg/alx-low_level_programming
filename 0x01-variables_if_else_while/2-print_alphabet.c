7e8ce500e81631a43f3ccf05a992035a7a4275ae
“gozie-gg” <goziegoodnews@gmail.com>
2022-12-11 17:18:28 -0800

    task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the alphabet in lowercase,
+ * main- prints the alphabet in lowercase,
  * followed by a new line
-  Return: Always 0 (Success)
-  */
+ *
+ * Return: Always 0 if successful, otherwise non-zero
+ */
 int main(void)
 {
 	char ch;
